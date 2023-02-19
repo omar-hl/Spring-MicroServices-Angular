cefba3482bf0a55f26876bcb9de341087aead60b
omar-hl <66702453+omar-hl@users.noreply.github.com>
2023-02-19 17:18:31 +0100

    first commit

---
+++ Billing-service/Billing-service/src/main/java/org/sid/billingservice/entities/ProductItem.java
@@ -0,0 +1,32 @@
+package org.sid.billingservice.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.billingservice.model.Product;
+
+import javax.persistence.*;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class ProductItem {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double quantity;
+    private double price;
+    private long productId;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToOne
+    private Bill bill;
+    @Transient
+    private Product product;
+    @Transient
+    private String productName;
+
+    public double getAmount()
+    {
+        return price * quantity;
+    }
+}
